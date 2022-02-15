670713169c2d73e6f6138b50f22aeb422a5e3b60
Ninomiya <hi@kan.run>
2022-02-15 21:42:42 +0900

    [fix]アイコンのサイズが大きすぎて表示されないことがあったのでファビコンサイズに変更

--- next.config.js
+++ next.config.js
@@ -5,6 +5,6 @@ module.exports = {
         serviceNamePlain: 'cloudbase', //大文字小文字を区別しない正式名称
         serviceDomain: 'cloudbase.host', //httpsや語尾の/無しのドメインのみ(例:pennso.com)
         serviceDescription: '🎛Control and integrate individual/group projects', //サービスの説明文を入力
-        serviceIcon: 'https://cloudbase.host/img/cloudbase-logo.png', //サービスのアイコンを指定するためのURL
+        serviceIcon: 'https://cloudbase.host/img/cloudbase-logo-favicon.png', //サービスのアイコンを指定するためのURL
     }
 };
