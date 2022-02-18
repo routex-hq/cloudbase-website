062a30248be3088fe7a145950c89ba07369debda
ninomiya <hi@kan.run>
2022-02-18 10:56:38 +0900

    [update] soonページの更新

--- src/pages/soon.tsx
+++ src/pages/soon.tsx
@@ -18,27 +18,19 @@ const Page = () => {
             <EditHead
             title={appName}
             description={appDesc}
-            url={"https://cloudbase.host/"} />
+            url={"https://cloudbase.host/soon/"} />
             <Header />
             <main className={styles.main}>
                 <div className={styles.container_top}>
                 <div className={styles.container_left}>
-                <h1 className={styles.title + ' ' + styles.back_green}>Focus With CloudBase</h1>
+                <h1 className={styles.title + ' ' + styles.back_green}>still a ways off</h1>
                 <p className={styles.subtitle + ' ' + styles.back_green}>you no longer have to worry about managing projects<br/>and can focus on dev.</p>
                 <div className={styles.buttons}>
-                    <Link href="/soon"><a><button className={styles.button_black}><span>Let's get right in<span><img src="https://s2.svgbox.net/hero-outline.svg?ic=arrow-right&color=fff" alt="" className={styles.arrow}/></span></span></button></a></Link>
-                    <Link href="/docs"><a><button className={styles.button_yellow}><span><img src="https://s2.svgbox.net/octicons.svg?ic=book&color=1A1A1A" className={styles.book}/></span><span>Documentation</span></button></a></Link>
-                </div>
-                <h2 className={styles.used_by + ' ' + styles.back_green}>used by</h2>
-                <div className={styles.used_by_logos}>
-                    <img src="/img/socialdog_inc_logo_white.svg" className={styles.social_dog} alt="socialdog"/>
-                    <img src="/img/CyberAgent_logo.svg" className={styles.cyberagent} alt="cyberagent"/>
-                    <img src="/img/Discord-Logo_Wordmark-Black.svg" className={styles.discord} alt="discord"/>
+                    <Link href="/soon"><a><button className={styles.button_black}><span>Subscribe<span><img src="https://s2.svgbox.net/hero-outline.svg?ic=arrow-right&color=fff" alt="" className={styles.arrow}/></span></span></button></a></Link>
                 </div>
                 </div>
                 <div className={styles.container_right}>
                     <div className={styles.video_box}>
-                        <YouTube videoId="dBOSUER_5T4" className={styles.video} />
                     </div>
                 </div>
                 </div>
@@ -61,6 +53,17 @@ const Page = () => {
                             <div className={styles.how_to_start_steps}>
                                 <h3 className={styles.back_green}>Create a project</h3>
                                 <p className={styles.back_green}>Create one project per app.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.how_to_start_box}>
+                    <div className={styles.how_to_start_wrap}>
+                    <h2 className={styles.title + ' ' + styles.back_green}>Investor</h2>
+                        <div className={styles.step_list}>
+                            <div className={styles.how_to_start_steps}>
+                                <h3 className={styles.back_green}>GV</h3>
+                                <p className={styles.back_green}>Google Venture</p>
                             </div>
                         </div>
                     </div>
